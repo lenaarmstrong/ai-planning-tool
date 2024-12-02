ca646f482eee43c9f8704fbf8bb3b3e66bc1478c
Lena Armstrong <lena318@sas.upenn.edu>
2024-12-02 15:33:00 -0500

    calendar fixes

--- calendar.py
+++ calendar.py
@@ -8,26 +8,26 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 
 # Resource: https://learndataanalysis.org/google-py-file-source-code/
-def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''):
-    CLIENT_SECRET_FILE = client_secret_file
-    API_SERVICE_NAME = api_name
-    API_VERSION = api_version
-    SCOPES = [scope for scope in scopes[0]]
+def create_service():
+    CLIENT_SECRET_FILE = 'client_secret.json'
+    API_NAME = 'calendar'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']   
     
     creds = None
     working_dir = os.getcwd()
     token_dir = 'token files'
-    token_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.json'
+    token_file = f'token_calendar_v3.json'
 
-    ### Check if token dir exists first, if not, create the folder
+    # Ensure token directory exists
     if not os.path.exists(os.path.join(working_dir, token_dir)):
         os.mkdir(os.path.join(working_dir, token_dir))
 
+    # Load existing credentials if available
     if os.path.exists(os.path.join(working_dir, token_dir, token_file)):
         creds = Credentials.from_authorized_user_file(os.path.join(working_dir, token_dir, token_file), SCOPES)
-        # with open(os.path.join(working_dir, token_dir, token_file), 'rb') as token:
-        #   cred = pickle.load(token)
 
+    # Refresh or reauthorize if credentials are invalid
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -39,22 +39,17 @@ def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''
             token.write(creds.to_json())
 
     try:
-        service = build(API_SERVICE_NAME, API_VERSION, credentials=creds, static_discovery=False)
-        print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
+        service = build(API_NAME, API_VERSION, credentials=creds, static_discovery=False)
+        print(API_NAME, API_VERSION, 'service created successfully')
         return service
     except Exception as e:
         print(e)
-        print(f'Failed to create service instance for {API_SERVICE_NAME}')
+        print(f'Failed to create service instance for calendar')
         os.remove(os.path.join(working_dir, token_dir, token_file))
         return None
 
 def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
-    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
-    return dt
+    date = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return date
 
-CLIENT_SECRET_FILE = 'client_secret.json'
-API_NAME = 'calendar'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+service = create_service()
