aa7acd853cd5dd1904e82fccfd3df63cefba5fc8
shanzehbatool <66784337+shanzehbatool@users.noreply.github.com>
2024-12-13 10:37:37 +0530

    Update llm_rag_chat.py

--- hci_user_study/src/api-service/api/routers/llm_rag_chat.py
+++ hci_user_study/src/api-service/api/routers/llm_rag_chat.py
@@ -18,26 +18,13 @@ async def perform_rag(request: QueryRequest):
     try:
         logger.info(f"Received query: {query}")
 
-        # RAG logic remains the same...
-        # persist_directory = "paper_vector_db/"
-        # model_name = "sentence-transformers/all-MiniLM-L6-v2"
-        project_id = "ai-research-for-good"
+        project_id = "###"
         location = "us-central1"
         model_id = "gemini-1.5-flash-002"
-        # TOP_K = 5
-
-        # bucket_name = "paper-rec-bucket"
-        # folder_prefix = "paper_vector_db/"
-        # destination_folder = "paper_vector_db"
-
-        # download_files_from_bucket(bucket_name, folder_prefix, destination_folder)
 
         logger.debug("Starting document retrieval")
-        # documents = retrieve_documents(query, persist_directory, model_name)
 
         logger.debug("Starting document ranking and filtering")
-        # model = GenerativeModel(model_id)
-        # top_documents = rank_and_filter_documents(query, documents, model, top_k=TOP_K)
 
         logger.debug("Starting answer generation")
         answer = generate_answer(query, project_id, location, model_id)
